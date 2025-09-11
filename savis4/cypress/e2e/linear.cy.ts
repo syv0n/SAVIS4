56dfe49480efbd46267abaee4b9ed28778e088cc
Shayn <voonshayn@gmail.com>
2025-09-11 12:31:14 -0700

    fixed errors in Run Dev Server and TESTS (tests portion should be fixed now) Firebase Deployment test now

--- savis4/cypress/e2e/linear.cy.ts
+++ savis4/cypress/e2e/linear.cy.ts
@@ -3,19 +3,19 @@ describe('template spec', () => {
         cy.viewport(1920, 1080)
         cy.visit('localhost:4200/LR')
     })
+    
     it('passes', () => {
         cy.viewport(1920, 1080)
         cy.visit('localhost:4200/LR')
-      })
+    })
+    
     it('should input data points and load the chart', () => {
         cy.get('#dataPoints').type('1,3 \n 2,5 \n 3,7 \n 4,9 \n 5,11')
-        cy.get('button.bg-white').click()
+        cy.get('button.btn').click()
         cy.wait(1000)
-        
-      })
-    it('should click the choose file button', () => {
-        cy.get('.file-input > .bg-white').click()
-      })
-
+    })
     
+    it('should click the choose file button', () => {
+        cy.get('.file-input .btn').click()
+    })
 })
