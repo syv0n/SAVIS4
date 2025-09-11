9a04bcfdbed595000c0bc619e7b28424d985e546
Shayn <voonshayn@gmail.com>
2025-09-11 12:03:07 -0700

    force commit

--- savis4/Savis4/cypress/e2e/spec.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.viewport(1920, 1080)
-    cy.visit('localhost:4200/')
-    // cy.get('.form-incline > #forget').click()
-  })
-})
--- savis4/Savis4/src/app/app-firebase.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { AngularFireModule } from "@angular/fire";
-import { environment } from "../environments/environment";
-
-@NgModule({
-    imports: [AngularFireModule.initializeApp(environment.firebaseConfig)],
-    exports: [AngularFireModule]
-})
-export class AppFirebaseModule {}
